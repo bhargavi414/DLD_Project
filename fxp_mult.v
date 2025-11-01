b6cbe8a022d6a50611cd5d74c1424884843c717a
RoopaReddy15 <roopakvsr@gmail.com>
2025-11-01 14:44:16 +0530

    Remove decoder module from fxp_mult.v

--- fxp_mult.v
+++ fxp_mult.v
@@ -1,18 +1,1 @@
 
-module decoder #(
-    parameter N = 2
-) (
-    input  wire [N-1:0] sel,
-    input wire  enable,
-    output reg [(1<<N)-1:0] out
-);
- integer i;
- always @(*) begin
-if (enable) begin
-out = { (1<<N){1'b0} }; 
-out[sel] = 1'b1;  
-end else begin
- out = { (1<<N){1'b0} };
-end
-end
-endmodule 
