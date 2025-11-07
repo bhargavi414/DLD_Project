b5a28fa36fe1f535d31d14885a36262fa5ae4fd0
bhargavi414 <bhargaviroutu224@gmail.com>
2025-11-08 00:12:37 +0530

    format code

--- fxp_ add.v
+++ fxp_ add.v
@@ -2,16 +2,11 @@
 // Adding two Numbers in the Q8.8 Format 8--> INTEGER PART & 8-->FRactional Part
 // Each number is represented as a 16-bit signed integer
 // By Usingf the Adder
-module add(a, b, sum);
- 
-  input [15:0] a; // Q8.8 format 1st number
-   
-   input [15:0] b; 
-  // Q8.8 format 2nd number  
-   
-    output [15:0] sum; 
-  // Q8.8 format sum of a and b
+module fxp_add #(parameter total_bits = 16)
 
-    assign sum = a + b; 
-  // Simple addition of two signed numbers and assigning the values
+(   input  signed  [total_bits-1:0] a, // Q8.8 format 1st number
+    input  signed  [total_bits-1:0] b,  // Q8.8 format 2nd number 
+    output signed  [total_bits-1:0] y   // Q8.8 format sum of a and b
+);
+    assign y = a + b; // Simple addition of two signed numbers and assigning the values
 endmodule
